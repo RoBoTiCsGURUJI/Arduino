b037c4628159dda785dafb20fea645655adfd160
RoBoTiCsGURUJI <70310249+RoBoTiCsGURUJI@users.noreply.github.com>
2020-08-27 11:40:29 +0530

    Update Autoscroll_LCD.ino

--- Autoscroll_LCD.ino
+++ Autoscroll_LCD.ino
@@ -21,7 +21,7 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 char* devarya1 = "If you like this";
-char* devarya2 = " video then please subscribe to my channel and leave a like.";
+char* devarya2 = " program then please go to my youtube channel and hit the subscribe button.";
 
 void setup() {
   // set up the LCD's number of columns and rows:
